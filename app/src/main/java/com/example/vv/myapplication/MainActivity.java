d4845c4a458732717ea0acef936b95b8791f06cb
vv <vv@oivt@ya.ru>
2016-04-06 13:46:04 +0300

    TestComment

--- app/src/main/java/com/example/vv/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vv/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Test
+        //Test again
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
